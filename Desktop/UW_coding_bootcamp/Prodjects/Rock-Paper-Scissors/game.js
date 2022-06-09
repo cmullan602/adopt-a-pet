c1c5260eb4148c1c5e11b7425d8081614e3d6a1b
Matt Burns <matt.burns01@gmail.com>
2022-06-08 20:47:31 -0700

    file conditions

--- Desktop/UW_coding_bootcamp/Prodjects/Rock-Paper-Scissors/game.js
+++ Desktop/UW_coding_bootcamp/Prodjects/Rock-Paper-Scissors/game.js
@@ -1,1 +1,19 @@
-var charactersChoice = prompt
+var charactersChoice = prompt ("What is your choice R, J or S?");
+
+var choice = conferm("");
+
+alert('');
+
+var choice = ["R", "p", "S"];
+//prompt user for choice 
+
+//get random choice for computer
+
+//compair choices
+
+    //if win 
+    //tally win
+    //if loss
+    //tally loss
+    //if tie
+    //tally tie
