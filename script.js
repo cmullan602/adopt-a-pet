d9d4a1b2489838c33093486abe2b0a20fccdc1bc
name <nameric@ymail.com>
2022-07-16 11:32:08 -0700

    worked on js api function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// var dogdummyApiKey = `9f30d9f26amsh0792997f4f9723dp171d82jsn8d8b6b792556`;
+// var dogdummyApiRootUrl = "https://dogdummyapi.p.rapidapi.com";
+
 var submitButton = $("#search");
+var searchInput = $("#search-input");
 
 submitButton.click(async function (e) {
   e.preventDefault();
@@ -6,27 +10,34 @@ submitButton.click(async function (e) {
   console.log("yes");
 });
 
-// async function api(url) {
-//   const response = await fetch(url);
-//   const json = await response.json();
-//   // api("someurl").then(function(res){console.log(res)};
+//api
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9f30d9f26amsh0792997f4f9723dp171d82jsn8d8b6b792556",
+    "X-RapidAPI-Host": "dogdummyapi.p.rapidapi.com",
+  },
+};
 
-//   return json;
-// }
-
-// api("http://dog-api.kinduff.comapi/facts?");
-
-//Get search input from user an well as location (zip code?)
-
-//Submit button
+fetch("https://dogdummyapi.p.rapidapi.com/dogs/", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
 
 //Render results
+function handleSearchSubmit(e) {
+  if (!searchInput.value) {
+    return;
+  }
 
+  e.preventDefault();
+  var search = searchInput.value.trim();
+  fetchCoords(search);
+  searchInput.value = "";
+}
 //Buttons to navigate results
 
 //Give user option to filter search results
-
-//Link results to Petfinder site
 
 //Fetch images from shiba API
 
