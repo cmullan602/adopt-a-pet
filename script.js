0abadc9fbadb6196bc40d790c1221e09ca9525d4
Connor Mullan <cmullan602@gmail.com>
2022-07-18 20:46:43 -0700

    added more images to dogs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var dogdummyApiKey = `9f30d9f26amsh0792997f4f9723dp171d82jsn8d8b6b792556`;
 var dogdummyApiRootUrl = "https://dogdummyapi.p.rapidapi.com";
-var dogUrl = "https://dog.ceo/api/breeds/image/random/3";
+var dogUrl = "https://dog.ceo/api/breeds/image/random/5";
 
 var submitButton = $("#submit-btn");
 var select = $("#dogs");
@@ -40,8 +40,8 @@ function getApiInfo(response) {
 
   $("#facts").html(
 
-    `<h2> Dog Facts<h2> 
-    <p> ${description}</p>`
+    `<h2>Dog Facts:<h2> 
+    <p>${description}</p>`
   );
   
 }
@@ -67,7 +67,9 @@ fetch(dogUrl, {
     $(".img-container").append(
       `<img id="column" src='${data.message[0]}'>
       <img id="column" src='${data.message[1]}'>
-      <img id="column" src='${data.message[2]}'>`
+      <img id="column" src='${data.message[2]}'>
+      <img id="column" src='${data.message[3]}'>
+      <img id="column" src='${data.message[4]}'>`
     );
   });
 
