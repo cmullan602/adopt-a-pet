ba798964638eda2078da522940568664fdf819f0
Connor Mullan <cmullan602@gmail.com>
2022-07-19 18:31:34 -0700

    fixed the buttons and added doge coin price tracker.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var dogdummyApiKey = `9f30d9f26amsh0792997f4f9723dp171d82jsn8d8b6b792556`;
 var dogdummyApiRootUrl = "https://dogdummyapi.p.rapidapi.com";
 var dogUrl = "https://dog.ceo/api/breeds/image/random/5";
 
+var dogAsAServiceApi = "ae97bf11-f094-404e-bf4f-08550f9ba818"
+
 var submitButton = $("#submit-btn");
 var select = $("#dogs");
 var dropdownMenu = "https://dogdummyapi.p.rapidapi.com/dogs/";
@@ -13,6 +15,22 @@ var selectBreed = "";
 var searchHistoryArr = [];
 var clearEl = $(".clear");
 
+const dogFacts = [
+  "Their sense of smell is at least 40x better than ours",
+  "Some have such good noses they can sniff out medical problems",
+  "Dogs can sniff at the same time as breathing",
+  "Some dogs are incredible swimmers",
+  "Some are fast and could even beat a cheetah!",
+  "Dogs don’t sweat like we do",
+  "Your dog could be left or right-pawed",
+  "Along with their noses, their hearing is super sensitive",
+  "Dogs have 18 muscles controlling their ears",
+  "Dogs are about as intelligent as a two-year-old"
+  ]
+  
+  let index = Math.floor(Math.random() * dogFacts.length)
+  dogFacts[index]
+
 
 // api
 var options = {
@@ -23,9 +41,10 @@ var options = {
   },
 };
 
-
+dogCeoRender(dogUrl)
 //Fetch images from Dog API
-fetch(dogUrl, {
+function dogCeoRender(url){
+fetch(url, {
   method: "GET",
   credentials: "same-origin",
   redirect: "follow",
@@ -42,7 +61,7 @@ fetch(dogUrl, {
       <img id="column" src='${data.message[4]}'>`
     );
   });
-
+}
 
 //generate list of dog names in the search bar
 function getApiInfo(response) {
@@ -79,22 +98,8 @@ function renderResponse(response){
 }
 
 
-function renderDescripFromSearch(name){
 
-  var apiUrl = `https://dogdummyapi.p.rapidapi.com/dog/name/${name}`
-  
-  fetch(apiUrl, options)
-      .then(function(response){
-          response.json().then(function (data){
-              console.log(data)
-              renderDescripFromSearch2(data)
-          })
-      })
-      
-}
-
-
-function renderDescripFromSearch2(dog){
+function renderDescripFromSearch(dog){
   var descrio= dog.description;
   var imagio = dog.image;
   console.log(descrio)
@@ -139,21 +144,26 @@ select.change(function () {
 });
 
 
-//render buttons 
+//render buttons of previous searches
 function renderSearches() {
   $(searchHistoryEL).empty();
   $(searchHistoryArr).each(function (i, searchHistory) {
-    $(searchHistoryEL).append(`
-        <button class="button btn-history is-full-width mt-2" data-search="${searchHistory}">${searchHistory}</button>
-        `)
-
-         $('.button').on('click', function() {
-            var string = $(this).attr('data-search')
-            console.log(string)
-            renderDescripFromSearch(string)
-          });
+    $(searchHistoryEL).append(
+      $("<button/>", {
+        text: searchHistory,
+        click: function (e) {
+          e.preventDefault()
+          var searchVal = $(this).text();
+          var queryURL = `https://dogdummyapi.herokuapp.com/dog/name/${searchVal}`;
+          fetch(queryURL, options)
+            .then((res) => res.json())
+            .then((res) => {
+              renderDescripFromSearch(res)
+            });
+        },
+      })
+    );
   });
-
 }
 
 
@@ -178,5 +188,41 @@ function setHistory(search) {
 
 renderSearches();
 
+var options2 = {
+  method: "GET",
+  headers: {
+    "Access-Control-Allow-Headers": "Content-Type"
+  },
+};
 
 
+
+    
+fetch('https://sochain.com//api/v2/get_price/DOGE/USD')
+              .then((response) => response.json())
+              
+              .then((response) => {
+                console.log(response)
+              renderDoge(response.data.prices[1])
+              })
+              
+              
+              .catch((err) => console.error(err));      
+              
+              
+function renderDoge(doge)  {
+  console.log(doge.price)
+  console.log(doge.time)
+
+  var dogePrice = doge.price 
+  var dogeTime = doge.time
+
+  $('.doge').append(`
+  <img src="Dogecoin_logo.0.jpeg"/>
+  <div class="column">
+  <p>Doge Coin Price: </p>
+  <p>${moment.unix(dogeTime).format("MM/DD/YYYY")}<p>
+  <p>$ ${dogePrice}<p>
+  </div>`)
+ 
+}
